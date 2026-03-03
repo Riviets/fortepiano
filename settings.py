59f09b3d61c545371e21bc2a990a2476e1d46c50
Riviets <vladvladpobuta@gmail.com>
2026-03-03 19:14:51 +0200

    Add settings and button effects

---
+++ settings.py
@@ -0,0 +1,18 @@
+from pygame import display
+
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 400
+screen = display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GREY = (200, 200, 200)
+
+KEYS = {
+    "a": "a6.mp3",
+    "b": "b6.mp3",
+    "c": "d6.mp3",
+    "d": "f6.mp3",
+    "e": "g6.mp3",
+    "f": "e6.mp3",
+    "g": "c6.mp3"
+}
